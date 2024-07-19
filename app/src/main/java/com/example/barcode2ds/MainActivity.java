353e0637e5b317304e0d9b1285aa12afaa67a031
Bao An <trieuhoangbaoan8@gmail.com>
2024-07-19 17:52:56 +0700

    +Mục giờ ghi +Mục ngày ghi

--- app/src/main/java/com/example/barcode2ds/MainActivity.java
+++ app/src/main/java/com/example/barcode2ds/MainActivity.java
@@ -1,136 +1,167 @@
 package com.example.barcode2ds;
 
-import android.Manifest;
-import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.os.Process;
-
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.DebugUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.rscja.barcode.BarcodeDecoder;
 import com.rscja.barcode.BarcodeFactory;
-import com.rscja.barcode.BarcodeUtility;
 import com.rscja.deviceapi.entity.BarcodeEntity;
- 
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-        Button btnScan;
-        Button btnStop;
-        TextView tvData;
-        String TAG="MainActivity_2D";
-        BarcodeDecoder barcodeDecoder= BarcodeFactory.getInstance().getBarcodeDecoder();
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnScan;
+    Button btnStop;
+    TextView tvData;
+    Spinner timeSpinner;
+    TextView dateTextView;
+    Calendar calendar;
 
+    String TAG = "MainActivity_2D";
+    BarcodeDecoder barcodeDecoder = BarcodeFactory.getInstance().getBarcodeDecoder();
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            btnScan=(Button)findViewById(R.id.btnScan);
-            tvData=(TextView)findViewById(R.id.tvData);
-            btnStop=(Button)findViewById(R.id.btnStop);
-            btnScan.setOnClickListener(this);
-            btnStop.setOnClickListener(this);
-            new InitTask().execute();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        btnScan = findViewById(R.id.btnScan);
+        tvData = findViewById(R.id.tvData);
+        btnStop = findViewById(R.id.btnStop);
+        btnScan.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+        // Spinner setup
+        timeSpinner = findViewById(R.id.spinner_time);
+        ArrayList<String> timeList = new ArrayList<>();
+        for (int i = 2; i <= 24; i += 2) {
+            timeList.add(i + ":00");
         }
+        ArrayAdapter<String> timeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, timeList);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
 
-        @Override
-        protected void onDestroy() {
-            Log.i(TAG,"onDestroy");
-            close();
-            super.onDestroy();
-            android.os.Process.killProcess(Process.myPid());
-        }
+        // DatePicker setup
+        dateTextView = findViewById(R.id.textview_date);
+        calendar = Calendar.getInstance();
+        updateDateLabel();
 
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-               case  R.id.btnScan:
-                   start();
-                break;
-                case  R.id.btnStop:
-                    stop();
-                break;
+        dateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
-        }
+        });
 
+        new InitTask().execute();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        close();
+        super.onDestroy();
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnScan:
+                start();
+                break;
+            case R.id.btnStop:
+                stop();
+                break;
+        }
+    }
+
+    private void start() {
+        barcodeDecoder.startScan();
+    }
+
+    private void stop() {
+        barcodeDecoder.stopScan();
+    }
+
+    private void open() {
+        barcodeDecoder.open(this);
+        Log.e(TAG, "open()==========================:" + barcodeDecoder.open(this));
+        barcodeDecoder.setDecodeCallback(new BarcodeDecoder.DecodeCallback() {
+            @Override
+            public void onDecodeComplete(BarcodeEntity barcodeEntity) {
+                Log.e(TAG, "BarcodeDecoder==========================:" + barcodeEntity.getResultCode());
+                if (barcodeEntity.getResultCode() == BarcodeDecoder.DECODE_SUCCESS) {
+                    tvData.setText("data:" + barcodeEntity.getBarcodeData());
+                    Log.e(TAG, "data==========================:" + barcodeEntity.getBarcodeData());
+                } else {
+                    tvData.setText("fail");
+                }
+            }
+        });
+    }
+
+    private void close() {
+        barcodeDecoder.close();
+    }
 
     public class InitTask extends AsyncTask<String, Integer, Boolean> {
-            ProgressDialog mypDialog;
-            @Override
-            protected Boolean doInBackground(String... params) {
-                // TODO Auto-generated method stub
-                open();
-                Log.e(TAG,"doInBackground==========================:");
-                return true;
-            }
-            @Override
-            protected void onPostExecute(Boolean result) {
-                super.onPostExecute(result);
-                mypDialog.cancel();
-            }
-            @Override
-            protected void onPreExecute() {
-                // TODO Auto-generated method stub
-                super.onPreExecute();
-                mypDialog = new ProgressDialog(MainActivity.this);
-                mypDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                mypDialog.setMessage("init...");
-                mypDialog.setCanceledOnTouchOutside(false);
-                mypDialog.setCancelable(false);
-                mypDialog.show();
-            }
+        ProgressDialog mypDialog;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            open();
+            Log.e(TAG, "doInBackground==========================:");
+            return true;
         }
 
-        private void start(){
-            barcodeDecoder.startScan();
-        }
-        private void stop(){
-            barcodeDecoder.stopScan();
-        }
-        private void open(){
-            barcodeDecoder.open(this);
-            Log.e(TAG,"open()==========================:"+ barcodeDecoder.open(this));
-           /* todo success Sound
-            BarcodeUtility.getInstance().enablePlaySuccessSound(this,true);
-            */
-            barcodeDecoder.setDecodeCallback(new BarcodeDecoder.DecodeCallback() {
-                @Override
-                public void onDecodeComplete(BarcodeEntity barcodeEntity) {
-                    Log.e(TAG,"BarcodeDecoder==========================:"+barcodeEntity.getResultCode());
-                    if(barcodeEntity.getResultCode() == BarcodeDecoder.DECODE_SUCCESS){
-                        tvData.setText("data:"+barcodeEntity.getBarcodeData());
-                        Log.e(TAG,"data==========================:"+barcodeEntity.getBarcodeData());
-                    }else{
-                        tvData.setText("fail");
-                    }
-                }
-            });
-
-        }
-        private void close(){
-            barcodeDecoder.close();
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            mypDialog.cancel();
         }
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mypDialog = new ProgressDialog(MainActivity.this);
+            mypDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mypDialog.setMessage("init...");
+            mypDialog.setCanceledOnTouchOutside(false);
+            mypDialog.setCancelable(false);
+            mypDialog.show();
+        }
+    }
 
+    private void updateDateLabel() {
+        String myFormat = "dd/MM/yyyy"; // In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateTextView.setText(sdf.format(calendar.getTime()));
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, monthOfYear);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDateLabel();
+        }
+    };
 }
