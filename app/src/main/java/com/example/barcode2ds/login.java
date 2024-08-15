0fefa35d3645ff9da2944a7ef51b66a77de25f40
Bao An <trieuhoangbaoan8@gmail.com>
2024-08-15 17:24:11 +0700

    +Xóa UI login

--- app/src/main/java/com/example/barcode2ds/login.java
+++ app/src/main/java/com/example/barcode2ds/login.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,6 +40,9 @@ public class login extends AppCompatActivity {
             finish();
             return;
         }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.login_layout);
 
