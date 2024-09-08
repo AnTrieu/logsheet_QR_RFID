d9eca843cf191ab01062410338532a51e7d3422c
Bao An <trieuhoangbaoan8@gmail.com>
2024-09-08 11:34:50 +0700

    +Giải quyết vấn đề cập nhật thời gian xảy ra lỗi PP: Định dạng lại mặc định dạng XX:00

--- app/src/main/java/com/example/barcode2ds/TimeHandler.java
+++ app/src/main/java/com/example/barcode2ds/TimeHandler.java
@@ -13,19 +13,17 @@ public class TimeHandler {
     public static void setupTimeAutoCompleteTextView(Context context, final AutoCompleteTextView timeACTV) {
         ArrayList<String> timeList = new ArrayList<>();
         for (int i = 2; i <= 24; i += 2) {
-            timeList.add(i + ":00");
+            timeList.add(String.format("%02d:00", i));
         }
 
         ArrayAdapter<String> timeAdapter = new ArrayAdapter<>(context, android.R.layout.simple_dropdown_item_1line, timeList);
         timeACTV.setAdapter(timeAdapter);
 
-        // Calculate the nearest even hour
         Calendar calendar = Calendar.getInstance();
         int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
         int nearestEvenHour = (currentHour % 2 == 0) ? currentHour : currentHour + 1;
 
-        // Set the default selection to the nearest even hour
-        String defaultTime = nearestEvenHour + ":00";
+        String defaultTime = String.format("%02d:00", nearestEvenHour);
         timeACTV.setText(defaultTime, false);
 
         timeACTV.setOnClickListener(new View.OnClickListener() {
